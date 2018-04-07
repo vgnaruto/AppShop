0367f5f2cbbe574471afd40d29e2746f1c01600f
Vincent Gunawan <caurnt@gmail.com>
2018-04-07 13:44:22 +0700

    layout
    membuat custom layout untuk action bar

--- app/src/main/java/com/example/windows10/appshop/ProductFragment.java
+++ app/src/main/java/com/example/windows10/appshop/ProductFragment.java
@@ -8,8 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class ProductFragment extends Fragment {
+public class ProductFragment extends Fragment implements View.OnClickListener{
+    private LinearLayout cartButton;
+    private TextView cartTv;
 
     public ProductFragment(){}
 
@@ -17,8 +21,24 @@ public class ProductFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_product,container, false);
+        this.cartButton = view.findViewById(R.id.button_cart);
+        this.cartTv = view.findViewById(R.id.tv_cart);
 
-
+        cartButton.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == cartButton){
+            String currentText = cartTv.getText().toString();
+            if(currentText.contains("ADD")){
+                cartButton.setBackgroundColor(getResources().getColor(R.color.gray_old));
+                cartTv.setText("REMOVE FROM CART");
+            }else if(currentText.contains("REMOVE")) {
+                cartButton.setBackgroundColor(getResources().getColor(R.color.orange));
+                cartTv.setText("ADD TO CART");
+            }
+        }
     }
 }
