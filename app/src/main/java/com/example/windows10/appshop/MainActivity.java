16a4ca40329f9a98208ebbcbb27cf2fb415ce282
Vincent Gunawan <caurnt@gmail.com>
2018-04-07 13:16:53 +0700

    Tampilan
    -proses membuat fragment product

--- app/src/main/java/com/example/windows10/appshop/MainActivity.java
+++ app/src/main/java/com/example/windows10/appshop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.windows10.appshop;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
